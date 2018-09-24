0fe161a9e7e88e3eb7adeb51b0633724158be6bf
Redcozmo <redcozmo@hotmail.com>
2018-09-24 14:23:17 +0200

    exercices jour jeudi THP-S0

---
+++ exo_05.rb
@@ -0,0 +1,25 @@
+# affichage a l'écran
+puts "On va compter le nombre d'heures de travail à THP"
+# affichage de la valeur de l'instruction entre {} directement après travail sans avoir besoin d'ajouter deux chaines de caractères : concaténation
+puts "Travail : #{10 * 5 * 11}"
+# meme principe
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}"
+# affiche la chaine de caractere
+puts "Et en secondes ?"
+# affiche le resultat de l'instruction
+puts 10 * 5 * 11 * 60 * 60
+# affiche la chaine de caractere
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
+# retourne true ou false en fonction de l'évaluation de l'intruction : false
+puts 3 + 2 < 5 - 7
+#etc
+puts "Ça fait combien 3 + 2 ? #{3 + 2}"
+puts "Ça fait combien 5 - 7 ? #{5 - 7}"
+
+puts "Ok, c'est faux alors !"
+
+puts "C'est drôle ça, faisons-en plus :"
+
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}"
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}"
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}"
---
+++ exo_09.rb
@@ -0,0 +1,9 @@
+puts "Bonjour, quel est ton prénom ?"
+print "> "
+user_name = gets.chomp
+
+puts "et ton nom de famille ?"
+print "> "
+user_lastname = gets.chomp
+
+puts "Bonjour #{user_name} #{user_lastname}"
